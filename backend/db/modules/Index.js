75e3ffd6225bf896fa75f816e54dc0fef17ce4f9
milenafmiteva <milenafmiteva@abv.bg>
2021-02-04 16:49:18 +0200

    Delete MongoDB, Connect with Firebase

--- backend/db/modules/Index.js
+++
@@ -1,7 +0,0 @@
-const { Board } = require('./Board.model');
-const { Task } = require('./Task.model');
-
-module.exports = {
-    Board,
-    Task
-}
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDx4jK0Su7vt0-Lma9szPWW3u4b4BBROHU",
+    authDomain: "trello-lite-5504a.firebaseapp.com",
+    projectId: "trello-lite-5504a",
+    storageBucket: "trello-lite-5504a.appspot.com",
+    messagingSenderId: "518648817839",
+    appId: "1:518648817839:web:054f4c77fd4ab1c58a0c5a",
+    measurementId: "G-9LNKCT4QC1"
+  }
 };
