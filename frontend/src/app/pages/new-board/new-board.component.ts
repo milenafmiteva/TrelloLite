30e0d40a59dc5b4f14293e0a9d858586da5ca538
milenafmiteva <milenafmiteva@abv.bg>
2021-02-06 20:29:30 +0200

    Style to be continued

---
+++ frontend/src/app/pages/new-board/new-board.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-board',
+  templateUrl: './new-board.component.html',
+  styleUrls: ['./new-board.component.scss']
+})
+export class NewBoardComponent implements OnInit {
+
+
+  ngOnInit(): void {
+  }
+
+  createBoard(title: string) {
+    
+  }
+}
